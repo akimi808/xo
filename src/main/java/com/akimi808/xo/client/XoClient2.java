46ba7677322d5ddeed73f1fec81276bdc665f658
Andrey Larionov <mou@yandex-team.ru>
2018-05-29 15:31:32 +0300

    Mockup for socket

--- src/main/java/com/akimi808/xo/client/XoClient2.java
+++ src/main/java/com/akimi808/xo/client/XoClient2.java
@@ -2,22 +2,26 @@ package com.akimi808.xo.client;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Queue;
 
+import com.akimi808.xo.common.Message;
 import com.akimi808.xo.common.Response;
 import com.akimi808.xo.common.Update;
+import com.akimi808.xo.server.MessageReader;
 
 /**
  * @author Andrey Larionov
  */
 public class XoClient2 {
     private SocketProcessor processor;
+
     public void connect() {
         try {
             SocketChannel socket = SocketChannel.open(new InetSocketAddress("localhost", 2810));
@@ -51,6 +55,11 @@ public class XoClient2 {
         private Selector readSelector;
         private Selector writeSelector;
         private boolean terminate = false;
+        private final ByteBuffer readBuffer = ByteBuffer.allocate(2048);
+        private final ByteBuffer writeBuffer = ByteBuffer.allocate(2048);
+        private final MessageReader messageReader = new MessageReader();
+        private final Queue<Message> outboundQueue
+        private boolean writeComplete;
 
         public SocketProcessor(SocketChannel socket) {
             this.socket = socket;
@@ -92,13 +101,35 @@ public class XoClient2 {
 
         private void readFromSocket() throws IOException {
             if (readSelector.selectNow() > 0) {
-
+                for (Iterator<SelectionKey> iterator = readSelector.selectedKeys().iterator(); iterator.hasNext(); ) {
+                    SelectionKey selectionKey = iterator.next();
+                    if (socket.read(readBuffer) == -1) {
+                        selectionKey.cancel();
+                        terminate = true;
+                    } else {
+                        List<Message> messages = messageReader.decodeMessage(readBuffer);
+                        for (Message message : messages) {
+                            // Handle messages
+                        }
+                    }
+                    iterator.remove();
+                    readBuffer.clear();
+                }
             }
-
         }
 
-        private void writeToSocket() {
+        private void writeToSocket() throws IOException {
+            boolean shouldSend = !outboundQueue.isEmpty() && !writeComplete;
+            if (!shouldSend) {
+                socket.keyFor(writeSelector).cancel();
+            } else {
+                writeComplete = false;
+                socket.register(writeSelector, SelectionKey.OP_WRITE);
+            }
 
+            if (writeSelector.selectNow() > 0) {
+                // Do write
+            }
         }
     }
 }
